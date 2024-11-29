89261a96f62d46a11aa39e41809c66adc9c93b23
Carlos Zamora <carlos14zamora@gmail.com>
2024-11-29 13:42:23 -0600

    add cors headers

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -37,7 +37,7 @@ const handleMqttData = async payload => {
     return;
   }
 
-  const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+  const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:[+-]\d{4}|Z)$/;;
   if (
     !arduinoTimestamp.match(dateFormat) ||
     !esp32Timestamp.match(dateFormat)
--- src/server.js
+++ src/server.js
@@ -10,6 +10,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
